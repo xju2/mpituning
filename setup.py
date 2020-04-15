cf63b668bdf7614f1f5938ddf89054c8ab189e01
Xiangyang Ju <xiangyang.ju@gmail.com>
2020-04-15 13:23:10 -0700

    add setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from setuptools import find_packages
+from setuptools import setup
+
+description="Tools for tuning multi-parton interactions in Pythia8"
+
+setup(
+    name="mpituning",
+    version="0.1.0",
+    description=description,
+    long_description=description,
+    author="HEPTrkx",
+    license="Apache License, Version 2.0",
+    keywords=["generator tuning"],
+    url="https://gitlab.cern.ch/xju/mpituning.git",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    setup_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+    ],
+    scripts=[
+        'scripts/change_yoda',
+        'scripts/compare_yoda_files',
+        'scripts/normalize_yoda',
+    ],
+)
